793ec2d1a40a6d5661b392286eab265cf76b0e0f
kadmy95@gmail.com <kadmy95@gmail.com>
2023-04-06 14:38:22 +0300

    bugs fixed

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import data from "./../../catalog.json";
 import { clearFilter, filterArray } from "./../store/cartSlice";
 import "./filter.css";
